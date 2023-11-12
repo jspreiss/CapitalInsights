638cfc94c13bf18c24a6f96b1fc0a085e15de2ad
Luke Ponssen <luke.ponssen@icloud.com>
2023-11-11 19:15:55 -0800

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let formData = ""; // Variable to store the form data
 
-// Call the INRIX API using the form data
-async function makeAnotherAPICall(formData) {
+// Find the latitude and longitude based on address
+async function findLatLong(address) {
 
-    //console.log('Form data within API call:', formData);
+    console.log('starting findLatLong with address:', address);
+
+    const geocodeAPIkey = "c61726c4e1c641249c8c39c657e51ea5";
+    // address for testing: "150+Valencia+St,+San%20Francisco,+CA+94103"
+    let url = `https://api.opencagedata.com/geocode/v1/json?key=${geocodeAPIkey}&q=${address.data}`;
+
+    var requestOptions = {
+        method: 'GET'
+    };
+
+    let latlong = await fetch(url, requestOptions);
+    let json = await latlong.json();
+    //console.log('this is the json:', json.results[0]);
+
+    let numResults = json.total_results;
+    if (numResults == 0) {
+        console.log("nothing found :(");
+    } else {
+        let coords = json.results[0].geometry;
+        //console.log('these are the coords:', coords);
+        return coords;
+    }
+
+    return coords;
+}
+
+// Call the INRIX API using the form data
+async function makeAnotherAPICall(coords) {
+
+    console.log('Coords used for INRIX API call:', coords);
     
     // Hard-coded authentication token
-    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjJoejdlNHRpeTMiLCJ0b2tlbiI6eyJpdiI6IjVmNjhiNzhlNGRhNWZhY2JiYmE2NjUxYWZkNzFhZjgzIiwiY29udGVudCI6IjQxMWNmODlhNmYwM2MwMmFhZjliYjcxOWU3OTMwMDE3YzExOGY2YzcyYjljNzM4NzE3YzBmNzFhZDg2NDAzMTJlODg0ODczN2NhNjNjYzY0MzUyNTIzNWZmMzdhOWU5Nzk3ZmJiYjA0NDA0NzVlNDBiZTQ0NTc1M2VlOWZjMDBhZGU2MWZkMjk5M2E3MDA4OTM2M2RmMzY3ZGU3MDdjOTA1YThkNDcyOTM1ODFhOTlkZmE0OTA3MGQyZmY2MTMyODE1YzRmYzQ4ZDhjOTIzYmFjNjUxZWQ3OGZmZjY0ODM5ZmQ1NzZiNGJjYTYyYWY0ZDA5Y2ViMGVlZWJiZTk5MzI3NWEwYzhjNjZkZThiZjgyY2U1MmMxZWQ0N2MwYmVjOWEyMDBlMTlmNTk3NjRkZjkwODgzMTQ2ZTRkMTE1OWJhYzJhYTRiNTZmZDE0NzViZWU3MWQ1YWI5YWNjZTU5MDZjYzQwZWUwZGRmMjg0MmQ5YTY1ZTNlMDQ2NDVmOTMwNjU1NGYwNzBhMDQwMzExZDYxZjRmYjc2MzI2NjBhNmMzZjRjMTI5YjMwMjliMmE2YTA4ZWZkZmU2ZWI2NmZhOWZmM2RkODNjY2Q4NWRiNTI3Yzk1MzZlNjA1Y2U5OTMwNDg2ZTkxMTFiMjM3MDBhNjkzNGJkYTUwNzczMDlmMTdmMjhhY2Y2YzI0NjkzZWNlMzk5NmQ4NWEwYzNmZDk3YjE3MDg5Mzc4Y2Y5NTlkMGEzZjhhNWNhMTJhNDYzY2VlOTgxN2YwZWUzZjc5NmEwMWExOGJmYWEwZjgwNTc2N2JjM2NiMDNhYzFhYzQ2MmExZGQ4YmFlMWFhMjM1N2Q3MjY4YTRmOGVjYTc0NzBiODcyYzRhZjRlIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI1ZjY4Yjc4ZTRkYTVmYWNiYmJhNjY1MWFmZDcxYWY4MyIsImNvbnRlbnQiOiI1NzRmODFjMjQ3M2NkYjI1YWI4MThkMDZkNjgzMGE1ZWU4NDhlY2U2MDc5YzEwZjUyN2I0Y2I2NmZhNDAwNjFmZDA4OGY0NDU5MjUyOGQ2MTI5N2UzZTYxIn0sImp0aSI6Ijg1MTBmZDgxLTk1YjgtNGI3My1iYWE5LTdhY2VmOTkwNDVhYiIsImlhdCI6MTY5OTc1MDAwNSwiZXhwIjoxNjk5NzUzNjA1fQ.vvWNYd-eGxPyn5z_1mxX78Duho-KrlN4prZyA-EHYzg";
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjJoejdlNHRpeTMiLCJ0b2tlbiI6eyJpdiI6IjQxYWI5MDBhMzlhYTQ0ODBlZDY5YWE0YTQ0YWYyYmYyIiwiY29udGVudCI6IjkwYjZmMDBkMjRlMzEwZDA5M2NiNjVmNWRhYjA5ODNjZDEwMTFiOGE4MjUxYmE1NDljMGEwNzMwYzU2MmYzNTQ2MWMyNmYwNDU0YmE1Yjc0Njg2YjEwMGFiZjlkMTNkOGEzZDkxNDFkYzQ1Y2U3NjkzOGY4ZWFkMDBjNWMxNWU5MGU4Y2MwZjYxNmQyNjc4N2RjYjUyZTQyYmVjN2MyMzk0YTlhNDI2YTRmYmQzMTc4ZjYyZjE1ZmY4NWQwNTk3NWJkMmNjZjliM2U3NDI3M2NkZDkwNzA0MzZjMGY1MTk0MzllMzM3NzNkYzcyYjM0YjYzNmIyNjk3ZDk3YjRmOTI1ZjcyMjJiNThkZjY3Zjg3OTJkYjZiNzQ5N2MwOGI3MmM5YmMxYWUxZTZjZTVjMDVlMTQyZTNhOWRmMzg3NjgxNDYxMzRkMDBkNjVkZGEzZTQxZDQ0MGM0MDAxN2UxMTYzMGUzZjNiYWM2ZmU5ZDU2NDRhYmY1YjAzNGUxNjIzZWM1YjIwMWZkY2E4YjQ1MGE0ZDZlYjRiNDhkMjFhNGY2YjU2MDc2ODE3NzQ5ZDEwODViNTRkNTZhYTEyMDI4YjI3MTFiNzNmOTA1MmQxM2U5ZjAyYzc1NzExMmIwNDE0ZjU4MGU3NDM4ZDQ3ZDVkMDcwY2M3YWY2ZGJmOWNhNjY3Mzc4ODY5N2U5ZWNhMDg4ZGUxY2E5NDMzZDE3YzMxNWE5OTU4N2I4ZmI2YTc5MjQ5YmIzMjE3ZTQwZWY1MTdjNTk3YjE2MTM4MmRiYjU1MjQ4YWY0NjcwYmYwN2NkM2YyY2VkYTYwMjEyYjgzMmJkNWY1YWRkODhkYjU2ZjYwYTcxMDE3OGFhZjNiYjFlNDNlODEzZDNhIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI0MWFiOTAwYTM5YWE0NDgwZWQ2OWFhNGE0NGFmMmJmMiIsImNvbnRlbnQiOiJiMTk2Y2Y0ZjFmY2YxOWZjYThmMDFkOGJkZTg1OTg0N2VkMmEyNzk3OTk0Njg3MzdhOTNkMGIzNGExNjRlZDRlNTNmMzUwN2Y0MzllNWE3OTQ2N2I2ODM0In0sImp0aSI6IjRkYjBiNzNjLTdhNTItNGI0Ny05N2IzLWNmMmZiM2VjNDdmNCIsImlhdCI6MTY5OTc1NzI2MSwiZXhwIjoxNjk5NzYwODYxfQ._rKRaX7aiO8vHFVRQjNfLfgaZXzuB6zkCxqgqjqDQvE";
     var myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${token}`);
 
@@ -30,8 +59,10 @@ async function makeAnotherAPICall(formData) {
 
     // Other parameters
     let radius = 100;
-    let coord1 = 37.781793;
-    let coord2 = -122.405189;
+    // let coord1 = 37.781793;
+    // let coord2 = -122.405189;
+    let coord1 = coords.lat; //37.7705928
+    let coord2 = coords.lng; //-122.40226318
 
     // Call the API with the url and fetch the data
     let url = `https://api.iq.inrix.com/v1/trips-count?od=destination&geoFilterType=circle&radius=${radius}m&points=${coord1}%7C${coord2}&limit=1000&startDateTime=%3E%3D2023-06-01T02%3A31&endDateTime=%3C%3D2023-06-15T02%3A31`;
@@ -46,9 +77,14 @@ async function makeAnotherAPICall(formData) {
     try {
 
       //console.log('Processing form data:', formData);
+
+      // Modify the input: replace all spaces with plus signs
+      let formattedaddress = formData.data.replace(/\s/g, '+');
+      console.log('new string', formattedaddress);
   
       // Make the API call
-      const apiResponse = await makeAnotherAPICall(formData);
+      const coords = await findLatLong(formData);
+      const apiResponse = await makeAnotherAPICall(coords);
 
       //console.log('Continuing processing with API response:', apiResponse);
       //console.log('Number of trips that', formData.data, 'made:', apiResponse.count);
